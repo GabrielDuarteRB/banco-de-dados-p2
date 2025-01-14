f24b06f83c81d27a1ca8174c2d5c20c5e21dea94
Gabriel Duarte <biel.dbastos@gmail.com>
2025-01-14 11:38:10 -0300

    Commit inicial

---
+++ app/entregas/funcoes.sql
@@ -0,0 +1,13 @@
+-- Primeira função
+
+CREATE OR REPLACE PROCEDURE convert_weight_to_kg()
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    UPDATE player
+    SET weight = weight * 0.453592
+    WHERE weight IS NOT NULL;
+END;
+$$;
+
+CALL convert_weight_to_kg();
