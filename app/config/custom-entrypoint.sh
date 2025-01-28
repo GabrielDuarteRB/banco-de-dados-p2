cc6c260ab9dc9bdf1ca4a74d081a7c0605581b27
Gabriel Duarte <biel.dbastos@gmail.com>
2025-01-28 17:23:47 -0300

    Monitoramento

--- app/config/custom-entrypoint.sh
+++ app/config/custom-entrypoint.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 set -e
 
-cp /usr/share/postgresql/postgresql.conf.sample /var/lib/postgresql/data/postgresql.conf
+cp /usr/share/postgresql/postgresql.conf /var/lib/postgresql/data/postgresql.conf
 cp /usr/share/postgresql/postgres.conf /etc/barman.d/postgres.conf
 
-echo "0 3 * * 1 barman backup postgres" > /etc/cron.d/barman-backup
-chmod 0644 /etc/cron.d/barman-backup
+chown postgres:postgres /var/lib/postgresql/data/postgresql.conf
+chown postgres:postgres /var/lib/postgresql/data/postgres.conf
 
+echo "0 3 * * 1 barman backup postgres" > /etc/cron.d/barman-backup
 echo "*/5 * * * * barman backup postgres" > /etc/cron.d/barman-incremental
+chmod 0644 /etc/cron.d/barman-backup
 chmod 0644 /etc/cron.d/barman-incremental
 
 service cron start
 
-exec /usr/local/bin/docker-entrypoint.sh postgres
+source /opt/temboard-venv/bin/activate
 
-tail -f /dev/null
+
+# echo 'aki'
+# temboard --home /etc/temboard/ generate-key
+
+# echo 'akii'
+# temboard --home /etc/temboard/ register-instance --host localhost --port 5432 --user postgres --password postgres
+
+# echo 'akiii'
+# temboard --home /etc/temboard/ serve &
+
